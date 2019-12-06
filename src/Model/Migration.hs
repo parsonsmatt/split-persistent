dc79089c0dd766a1681952b0df25879696d1ef6e
parsonsmatt <parsonsmatt@gmail.com>
2019-12-06 15:54:07 -0700

    Split Models

--- src/Model/Migration.hs
+++ src/Model/Migration.hs
@@ -7,9 +7,16 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-module Model (module X) where
 
-import Model.User as X
-import Model.Comment as X
-import Model.Email as X
-import Model.Migration as X
+module Model.Migration where
+
+import System.Directory
+import ClassyPrelude.Yesod
+import Database.Persist.Quasi
+
+mkMigrate "migrateAll" $(do
+    files <- liftIO $ do
+        dirContents <- getDirectoryContents "config/models/"
+        pure $ map ("config/models/" <>) $ filter (".persistentmodels" `isSuffixOf`) dirContents
+    persistManyFileWith lowerCaseSettings files
+    )
